f598af2c5acc32b3e64a397b664777db923ec1e0
lex_s <lex.suleimanov@gmail.com>
2018-04-03 17:19:02 +0300

    JADE launched!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import jade.core.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Agent a = new Agent();
         System.out.println("Hello World!");
     }
 }
